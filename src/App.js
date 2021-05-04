12c4e20d57c9b4f01d207e0bceabea75fd80d6ec
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-04 19:26:27 -0400

    working on nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import About from './components/About'
-import Contact from './components/Contact'
+import Header from './components/Header';
+import Nav from './components/Nav';
+import About from './components/About';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
@@ -8,6 +10,9 @@ import Resume from './components/Resume';
 function App() {
   return (
     <div className="App">
+      <Header>
+        <Nav></Nav>
+      </Header>
       <main>
         <About />
         <Contact />
