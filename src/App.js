c789b517d0605847be230c8657023b3b338c9bf5
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-03 23:49:06 -0400

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   return (
     <div className="App">
       <main>
         <About />
+        <Contact />
       </main>
     </div>
   );
