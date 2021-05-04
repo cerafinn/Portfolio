5435241f264d8fac7e8f8b73e33a07f212cf91c0
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-04 16:18:52 -0400

    working on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,18 +10,14 @@ function Footer() {
 
   return (
     <footer>
-              {currentPhotos.map((image, i) => (
-          <img
-            alt={image.name}
-            src={require(`../../assets/small/${category}/${i}.jpg`).default}
-            className="img-thumbnail mx-1"
-            key={image.name}
-            onClick={() => toggleModal(image, i)}
-          />
+      {accounts.map(account => (
+        <a
+        href={account.link}
+        key={account.name}
+        target="_blank" rel="noreferrer"
+        >
+        <i className={account.icon}></i></a>
         ))}
-      | <a href="http://github.com/cerafinn"><i class="devicon-github-original"></i></a> 
-      | <a href="https://www.linkedin.com/in/andaleebfarooq/"><i class="devicon-linkedin-plain"></i></a>
-      | <a href="https://twitter.com/cerahime"><i class="devicon-twitter-original"></i></a>
     </footer>
   )
 }
