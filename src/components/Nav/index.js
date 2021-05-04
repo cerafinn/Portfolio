ae0c234751012ed425043910be9f1c40ef876574
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-04 18:57:56 -0400

    working on nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import capitalizeFirstLetter from '../../utils/helpers';
 
-function Nav() {
+function Nav(currentPage) {
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentPage.name);
+  }, [currentPage]);
+
+  <ul className="flex-row">
+  <li className="mx-2">
+    <a data-testid="about" href="/about">About Me</a>
+  </li>
+  <li className="mx-2">
+    <a href="/portfolio">Portfolio</a>
+  </li>
+  <li className="mx-2">
+    <a href="/contact">Contact Me</a>
+  </li>
+  <li className="mx-2">
+    <a href="/resume">Resume</a>
+  </li>
+</ul>
+
     return (
     <header className="flex-row px-1">
-      <h2>
-        <a data-testid="link" href="/">
-          <span role="img" aria-label="name">Andaleeb Farooq</span>
-        </a>
-      </h2>
       <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="/about">About Me</a>
-          </li>
-          <li className="mx-2">
-            <a href="/portfolio">Portfolio</a>
-          </li>
-          <li className="mx-2">
-            <a href="/contact">Contact Me</a>
-          </li>
-          <li className="mx-2">
-            <a href="/resume">Resume</a>
-          </li>
-        </ul>
-      </nav>
+      <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">Andaleeb Farooq</a>
+      <a href="/#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+      </div>
+    </nav>
+
+      <ul class="sidenav" id="mobile-demo">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
     </header>
   );
 };
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,34 +1,48 @@
 import React from 'react';
 
 function Resume() {
-  const technologies = [
+  const frontendTechnologies = [
     { name: "HTML" , icon: "devicon-html5-plain-wordmark colored" },
     { name: "CSS" , icon: "devicon-css3-plain-wordmark colored" },
     { name: "JS" , icon: "devicon-javascript-plain colored" },
-    { name: "Node" , icon: "devicon-nodejs-plain-wordmark colored" },
     { name: "Heroku" , icon: "devicon-heroku-plain-wordmark colored" },
     { name: "Express" , icon: "devicon-express-original-wordmark" },
     { name: "Handlebars" , icon: "devicon-handlebars-plain-wordmark colored" },
+    { name: "React" , icon: "devicon-react-original-wordmark colored" },
+    // { name: "" , icon: "" },
+  ]
+
+  const backendTechnologies = [
+    { name: "Node" , icon: "devicon-nodejs-plain-wordmark colored" },
+    { name: "Heroku" , icon: "devicon-heroku-plain-wordmark colored" },
+    { name: "Express" , icon: "devicon-express-original-wordmark" },
     { name: "jQuery" , icon: "devicon-jquery-plain-wordmark colored" },
     { name: "MySQL" , icon: "devicon-mysql-plain colored" },
     { name: "MongoDB" , icon: "devicon-mongodb-plain-wordmark colored" },
     { name: "NPM" , icon: "devicon-npm-original-wordmark colored" },
-    { name: "React" , icon: "devicon-react-original-wordmark colored" },
-    // { name: "" , icon: "" },
     { name: "Git" , icon: "devicon-git-plain-wordmark colored" }
-
+    // { name: "" , icon: "" },
   ]
 
   return (
     <div className>
       <h2>proficiencies;;</h2>
-      <section>
-        <div>
-          {technologies.map(technology => (
-          <i className={technology.icon}></i>
-          ))}
+      <section className="row">
+        <div className="col-md">
+          <ul>
+            {frontendTechnologies.map(technology => (
+            <li><i className={technology.icon}></i></li>
+            ))}
+          </ul>
         </div>
-        <div>
+        <div className="col-md">
+          <ul>
+            {backendTechnologies.map(technology => (
+            <li><i className={technology.icon}></i></li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-md">
           You can find my resume <a href="/">here</a>
         </div>
       </section>
