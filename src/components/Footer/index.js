2040b336f44488e1abda3f9f0676fe5ebfedf5f2
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-04 16:12:16 -0400

    working on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 function Footer() {
+  const accounts = [
+    { name: "Github" , link: "http://github.com/cerafinn", icon: "devicon-github-original" },
+    { name: "LinkedIn" , link: "https://www.linkedin.com/in/andaleebfarooq/", icon: "devicon-linkedin-plain" },
+    { name: "Twitter" , link: "https://twitter.com/cerahime", icon: "devicon-twitter-original" }
+    // For future accounts: { name: "" , link: "", icon: "" }
+  ]
 
   return (
     <footer>
+              {currentPhotos.map((image, i) => (
+          <img
+            alt={image.name}
+            src={require(`../../assets/small/${category}/${i}.jpg`).default}
+            className="img-thumbnail mx-1"
+            key={image.name}
+            onClick={() => toggleModal(image, i)}
+          />
+        ))}
       | <a href="http://github.com/cerafinn"><i class="devicon-github-original"></i></a> 
       | <a href="https://www.linkedin.com/in/andaleebfarooq/"><i class="devicon-linkedin-plain"></i></a>
       | <a href="https://twitter.com/cerahime"><i class="devicon-twitter-original"></i></a>
