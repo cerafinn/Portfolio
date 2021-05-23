bcd5ce6a5bfbc8df6b74c282dbe8ca39d8ec7cf8
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-22 23:15:09 -0400

    added photo and corrected portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,43 +4,50 @@ function Portfolio() {
   const [projects] = useState([
     {
       name: 'Currenseed',
-      repo: 'https://github.com/DJ-KARMA/currenseed',
+      repo: 'currenseed',
+      link: 'https://github.com/DJ-KARMA/currenseed',
       deployed: 'https://currenseed.herokuapp.com/',
       description: 'Project',
     },
     {
       name: 'Reward Bank',
-      repo: 'https://github.com/UTBootCampGroup2Project2/reward-bank',
+      repo: 'reward-bank',
+      link: 'https://github.com/UTBootCampGroup2Project2/reward-bank',
       deployed: 'https://rocky-dawn-18669.herokuapp.com/#',
       description: 'Project',
     },
     {
       name: 'Covid Dashboard',
-      repo: 'https://github.com/Runtime-Terrors-Organization/covid-dashboard-application',
+      repo: 'covid-dashboard-application',
+      link: 'https://github.com/Runtime-Terrors-Organization/covid-dashboard-application',
       deployed: 'https://runtime-terrors-organization.github.io/covid-dashboard-application/',
       description: 'Project',
     },
     {
       name: 'Code Quiz',
-      repo: 'https://github.com/cerafinn/code-quiz',
+      repo: 'code-quiz',
+      link: 'https://github.com/cerafinn/code-quiz',
       deployed: 'https://cerafinn.github.io/code-quiz',
       description: 'JavaScript',
     },
     {
       name: 'Day Scheduler',
-      repo: 'https://github.com/cerafinn/day-scheduler',
+      repo: 'day-scheduler',
+      link: 'https://github.com/cerafinn/day-scheduler',
       deployed: 'https://cerafinn.github.io/day-scheduler/',
       description: 'jQuery',
     },
     {
       name: 'Horiseon',
-      repo: 'https://github.com/cerafinn/horiseon',
+      repo: 'horiseon',
+      link: 'https://github.com/cerafinn/horiseon',
       deployed: 'https://cerafinn.github.io/Horiseon/',
       description: 'Code Refactor',
     },
     {
       name: 'Run Buddy',
-      repo: 'https://github.com/cerafinn/run-buddy',
+      repo: 'run-buddy',
+      link: 'https://github.com/cerafinn/run-buddy',
       deployed: 'https://cerafinn.github.io/run-buddy/',
       description: 'HTML/CSS',
     }
@@ -53,7 +60,7 @@ function Portfolio() {
         <div className="collection-grid">
           {projects.map((project) => (
             <div className="project">
-              <img src={require(`../../assets/projects/${project.repo}.png`).default} alt={project.name} className="project-img" />
+              <img src={require(`../../assets/projects/${project.link}.png`).default} alt={project.name} className="project-img" />
             <div className="project-info">
               <h3>
                 <a href={project.deployed} target="_blank" rel="noreferrer">{project.name}</a>{' - '}<a href={project.repo} target="_blank" rel="noreferrer">Repo</a>
