9a07a93f9eea93b1235e642d007629a838a9bef3
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-06-17 15:20:20 -0400

    added mail option to portfolio

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,8 @@ function Footer() {
   const accounts = [
     { name: "Github" , link: "http://github.com/cerafinn", icon: "devicon-github-original" },
     { name: "LinkedIn" , link: "https://www.linkedin.com/in/andaleebfarooq/", icon: "devicon-linkedin-plain" },
-    { name: "Twitter" , link: "https://twitter.com/cerahime", icon: "devicon-twitter-original" }
+    { name: "Twitter" , link: "https://twitter.com/cerahime", icon: "devicon-twitter-original" },
+    { name: "Email" , link: "mailto:andaleeb.farooq@gmail.com", icon: "far fa-envelope" }
     // For future accounts: { name: "" , link: "", icon: "" }
   ]
 
